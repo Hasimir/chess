fcb184b5199982f227984572bfe8eaa9a07ac73b
Ben McGinnes <ben@adversary.org>
2014-09-10 02:23:37 +1000

    Initial generic loader.  More details to come.

---
+++ UCI/UCIloader/uciloader.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+ 
+int main()
+{
+  FILE *f = fopen("wrapper.ini");
+  char buf[1024], exe_name[1024];
+  if(!f) return -1;
+  fscanf(f, "%[^\n]", exe_name); fclose(f);
+  snprintf(buf, 1024, "/opt/local/bin/wine %s/%s", get_current_dir_name(), exe_name);
+  system(buf);
+  return 0;
+}
