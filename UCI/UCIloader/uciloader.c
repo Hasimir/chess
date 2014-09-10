9d4f00a7f7d56c493271857f5461f742a9d99111
Ben McGinnes <ben@adversary.org>
2014-09-10 14:05:08 +1000

    C code works on Linux, but not yet on OS X.

--- UCI/UCIloader/uciloader.c
+++ UCI/UCIloader/uciloader.c
@@ -4,11 +4,11 @@
  
 int main()
 {
-  FILE *f = fopen("wrapper.ini");
+  FILE *f = fopen("wrapper.ini", "r");
   char buf[1024], exe_name[1024];
   if(!f) return -1;
   fscanf(f, "%[^\n]", exe_name); fclose(f);
   snprintf(buf, 1024, "/opt/local/bin/wine %s/%s", get_current_dir_name(), exe_name);
   system(buf);
   return 0;
-}
+} 
