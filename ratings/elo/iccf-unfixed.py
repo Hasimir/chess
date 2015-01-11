2588b33d7f3823d507be2a7aa7d5e2eee8dd6179
Ben McGinnes <ben@adversary.org>
2015-01-11 13:08:09 +1100

    Unknown number of changes, probably building the table of values for D(p).

--- ratings/elo/iccf-unfixed.py
+++ ratings/elo/iccf-unfixed.py
@@ -7,14 +7,30 @@ Rc = int(input("Enter the current rating: "))
 games = input("Enter the number of games: ")
 wins = input("Enter the number of wins: ")
 draws = input("Enter the number of draws: ")
-rsum = input("Enter sum of all opponents' ratings: ")
 
-g = float(games)
+g = int(games)
 w = float(wins)
 d = float(draws)
 p = (w + d) / g
-# Dp = ?
+
+a = []
+
+for i in range(g):
+    r = int(input("Enter the opponent's rating for game {0}: ".format(i + 1)))
+    a.insert(0, r)
+
+rsum = sum(a)
+
 # Fp = ?
+
+if 0.99 <= p <= 1:
+    Dp = 677
+elif 0.98 <= p < 0.99:
+    Dp = 589
+elif 0.97 <= p < 0.98:
+    Dp = 538
+elif 0.96 <= p < 0.97:
+    Dp = 501
 
 Rc = float(rsum) / g
 #Rn = Rc + Dp * Fp
